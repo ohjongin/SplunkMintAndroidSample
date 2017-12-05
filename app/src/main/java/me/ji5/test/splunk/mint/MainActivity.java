13a4f5da508d9703cea49e056a2de6de7f161d74
Jongin Oh <ohjongin@gmail.com>
2017-12-05 13:36:31 +0900

    Add a button to generate crash

--- app/src/main/java/me/ji5/test/splunk/mint/MainActivity.java
+++ app/src/main/java/me/ji5/test/splunk/mint/MainActivity.java
@@ -2,10 +2,12 @@ package me.ji5.test.splunk.mint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.splunk.mint.Mint;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Mint.initAndStartSession(this.getApplication(), "");
+
+        initViews();
+    }
+
+    protected void initViews() {
+        Button btnCrash = findViewById(R.id.btn_crash);
+        btnCrash.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_crash:
+                onBtnCrash();
+                break;
+        }
+    }
+
+    protected void onBtnCrash() {
+        throw new RuntimeException("User generate a crash !!!");
     }
 }
